0dbf502d7e8abd5ef06d9df61078c01aeb8d5693
BONNe <bonne@bonne.id.lv>
2025-08-24 19:12:27 +0300

    Convert to paper-plugin.

---
+++ src/main/java/lv/id/bonne/animalpenpaper/AnimalPenBootstrap.java
@@ -0,0 +1,14 @@
+package lv.id.bonne.animalpenpaper;
+
+
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+
+
+public class AnimalPenBootstrap implements PluginBootstrap
+{
+    @Override
+    public void bootstrap(BootstrapContext context)
+    {
+    }
+}
